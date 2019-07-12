8ea836aedec6810f41734506168602060a20a408
makarenkoj <makarenkoj@gmail.com>
2019-07-12 12:59:04 +0300

    initial commit

---
+++ word_reader.rb
@@ -0,0 +1,21 @@
+# encoding: UTF-8
+
+class WordReader
+
+  def read_from_args
+    return ARGV[0]
+  end
+
+  def read_from_file(file_name)
+
+    if !File.exist?(file_name)
+      return nil
+    end
+
+    file = File.new(file_name, "r:UTF-8")
+    lines = file.readlines
+    file.close
+
+    return lines.sample.chomp
+  end
+end
