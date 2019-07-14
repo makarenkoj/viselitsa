b0f3b7d107206bd8024559902aebed11ccaedd58
makarenkoj <makarenkoj@gmail.com>
2019-07-14 13:17:53 +0300

    modified word_reader

--- word_reader.rb
+++ word_reader.rb
@@ -4,6 +4,7 @@ class WordReader
   def read_from_args
     ARGV[0]
   end
+
   def read_from_file(file_name)
     if !File.exist?(file_name)
       return nil
@@ -11,6 +12,7 @@ class WordReader
     file = File.new(file_name, "r:UTF-8")
     lines = file.readlines
     file.close
+
     lines.sample.chomp
   end
 end
