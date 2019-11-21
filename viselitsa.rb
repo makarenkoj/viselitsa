f85b180b4fd6718b1cdaca7bbbb86de88f87277a
makarenkoj <makarenkoj53@gmail.com>
2019-11-21 15:08:51 +0200

    add lib directory

--- viselitsa.rb
+++ viselitsa.rb
@@ -1,30 +1,23 @@
-# encoding: UTF-8
+# encoding: utf-8
 
-if Gem.win_platform?
-  Encoding.default_external = Encoding.find(Encoding.locale_charmap)
-  Encoding.default_internal = __ENCODING__
+require 'unicode_utils/upcase'
 
-  [STDIN, STDOUT].each do |io|
-    io.set_encoding(Encoding.default_external, Encoding.default_internal)
-  end
-end
+require_relative 'lib/game'
+require_relative 'lib/result_printer'
+require_relative 'lib/word_reader'
 
-require_relative "game"
-require_relative "result_printer"
-require_relative "word_reader"
-
-puts "Игра виселица. Версия 3. (c) goodprogrammer.ru\n\n"
-sleep 1
-
-printer = ResultPrinter.new
+VERSION = 'Игра виселица, версия 5. (c) Хороший программист'
 
 word_reader = WordReader.new
+words_file_name = "#{File.dirname(__FILE__)}/data/words.txt"
+word = word_reader.read_from_file(words_file_name)
 
-words_file_name = File.dirname(__FILE__) + "/data/words.txt"
+game = Game.new(word)
+game.version = VERSION
 
-game = Game.new(word_reader.read_from_file(words_file_name))
+printer = ResultPrinter.new(game)
 
-while game.status == 0
+while game.in_progress?
   printer.print_status(game)
   game.ask_next_letter
 end
